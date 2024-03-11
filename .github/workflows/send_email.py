aa5ec0572278a6ddde5c90899932f812079671bf
surajvishwasbondre <114866975+surajvishwasbondre@users.noreply.github.com>
2024-03-11 18:06:18 +0530

    Update send_email.py

--- .github/workflows/send_email.py
+++ .github/workflows/send_email.py
@@ -1,6 +1,11 @@
-import smtplib, ssl
+import smtplib
+import ssl
 import os
 
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+# Construct the full path to the send_email.py file
+send_email_path = os.path.join(script_dir, 'send_email.py')
 
 port = 465
 smtp_server = "smtp.gmail.com"
@@ -13,6 +18,10 @@ This is your daily email report.
 """
 
 context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(USERNAME,PASSWORD)
-    server.sendmail(USERNAME,USERNAME,message)
+
+try:
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(USERNAME, PASSWORD)
+        server.sendmail(USERNAME, USERNAME, message)
+except Exception as e:
+    print(f"An error occurred: {e}")
